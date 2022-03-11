11d4da0022c20ceb1e2c7426e03fe06d1787c1d6
Lyle Cheatham <66628590+Lyle-Alloy@users.noreply.github.com>
2022-03-11 12:12:00 -0500

    Update hook-pygraphviz.py

--- src/_pyinstaller_hooks_contrib/hooks/stdhooks/hook-pygraphviz.py
+++ src/_pyinstaller_hooks_contrib/hooks/stdhooks/hook-pygraphviz.py
@@ -47,19 +47,36 @@ if is_win:
     for data in glob.glob("c:/Program Files/Graphviz*/bin/config*"):
         datas.append((data, "."))
 else:
-    # The dot binary in PATH is typically a symlink, handle that.
-    # graphviz_bindir is e.g. /usr/local/Cellar/graphviz/2.46.0/bin
-    graphviz_bindir = os.path.dirname(os.path.realpath(shutil.which("dot")))
-    for binary in progs:
-        binaries.append((graphviz_bindir + "/" + binary, "."))
+    # The dot binary in PATH is typically a symlink, and on darwin it links to a binary
+    # that is properly named (i.e. /usr/local/bin/dot ->
+    # /usr/local/Cellar/graphviz/2.46.0/bin/dot), but on linux it sometimes symlinks to a
+    # *differently named binary* (i.e. /usr/bin/dot -> /usr/sbin/libgvc6-config-update).
+    # This means we have to handle these differently.
     if is_darwin:
+        # On darwin we want to find the real directory because the libraries are relative
+        # to that directory
+
+        # graphviz_bindir is e.g. /usr/local/Cellar/graphviz/2.46.0/bin
+        graphviz_bindir = os.path.dirname(os.path.realpath(shutil.which("dot")))
+        for binary in progs:
+            binaries.append((graphviz_bindir + "/" + binary, "."))
+
         suffix = "dylib"
         # graphviz_libdir is e.g. /usr/local/Cellar/graphviz/2.46.0/lib/graphviz
         graphviz_libdir = os.path.realpath(graphviz_bindir + "/../lib/graphviz")
     else:
+        # On linux we don't actually care to find the real directory because we call
+        # findLibrary to sort things out anyway, so just get the symlink directory
+
+        # graphviz_bindir is e.g. /usr/bin
+        graphviz_bindir = os.path.dirname(shutil.which("dot"))
+        for binary in progs:
+            binaries.append((graphviz_bindir + "/" + binary, "."))
+
         suffix = "so"
         # graphviz_libdir is e.g. /usr/lib64/graphviz
-        graphviz_libdir = os.path.join(os.path.dirname(findLibrary('libcdt')), 'graphviz')
+        graphviz_libdir = os.path.join(os.path.dirname(findLibrary("libcdt")), "graphviz")
+
     for binary in glob.glob(graphviz_libdir + "/*." + suffix):
         binaries.append((binary, "graphviz"))
     for data in glob.glob(graphviz_libdir + "/config*"):
